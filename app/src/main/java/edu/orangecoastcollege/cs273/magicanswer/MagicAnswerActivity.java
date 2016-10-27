47c0b9aa2aecd1076c8047d5b4e5cc82d1f6b22c
kfrederick5 <tefsuncle@gmail.com>
2016-10-27 16:34:47 -0700

    Finished project

--- app/src/main/java/edu/orangecoastcollege/cs273/magicanswer/MagicAnswerActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/magicanswer/MagicAnswerActivity.java
@@ -1,13 +1,27 @@
 package edu.orangecoastcollege.cs273.magicanswer;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MagicAnswerActivity extends AppCompatActivity {
 
     MagicAnswer magicAnswer;
+    private EditText questionEditText;
     private TextView answerTextView;
+
+    //Reference to SensorManager (manages ALL sensors on device)
+    private SensorManager sensorManager;
+
+    //Ref to accelerometer
+    private Sensor accelerometer;
+
+    //Ref to shake detector
+    private ShakeDetector shakeDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +30,36 @@ public class MagicAnswerActivity extends AppCompatActivity {
 
         // TASK 1: SET THE REFERENCES TO THE LAYOUT ELEMENTS
         answerTextView = (TextView) findViewById(R.id.answerTextView);
+        questionEditText = (EditText) findViewById(R.id.questionEditText);
 
         // TASK 2: CREATE A NEW MAGIC ANSWER OBJECT
+        magicAnswer = new MagicAnswer(this);
 
         // TASK 3: REGISTER THE SENSOR MANAGER AND SETUP THE SHAKE DETECTION
+        sensorManager =  (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        shakeDetector = new ShakeDetector(new ShakeDetector.OnShakeListener() {
+            @Override
+            public void onShake() {
+                // Get random answer, put it in the answerTextView
+                String answer = magicAnswer.getRandomAnswer();
+                answerTextView.setText(answer);
+            }
+        });
 
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //Start accelerometer
+        sensorManager.registerListener(shakeDetector, accelerometer, SensorManager.SENSOR_DELAY_UI);
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(shakeDetector);
+    }
 }
